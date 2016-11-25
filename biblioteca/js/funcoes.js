531ac0a397c9ba12b72766ae81fdca6a50230d01
Diogo <dpaniz1@ucs.br>
2016-11-25 09:03:56 -0200

    ramo diogo

--- biblioteca/js/funcoes.js
+++ biblioteca/js/funcoes.js
@@ -6,7 +6,9 @@
  - function abrePopup(url,nome,w,h,s)
  - function validaForm(formulario)
  *******************************************************************************/
-
+ function ajuda(confirmMsg){ 
+	return alert (confirmMsg);;
+}
 <<<<<<< HEAD
  function desliga(confirmMsg){ 
 	return alert (confirmMsg);;
